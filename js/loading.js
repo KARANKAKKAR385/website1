4438f8f05b9bdb5d79711e974525f3bf32563615
KARANKAKKAR385 <143103376+KARANKAKKAR385@users.noreply.github.com>
2024-03-08 22:45:12 +0000

    Fri, Mar 8, 2024, 2:45 PM -08:00

---
+++ js/loading.js
@@ -0,0 +1,9 @@
+loadText = document.querySelector('.loading-text');
+bg = document.querySelector('.bg')
+load = 90
+
+loadText.innerText = `${load}%`
+loadText.style.style.opacity = 0.1
+bg.style.filter = "blur(10px)"
+}
+
