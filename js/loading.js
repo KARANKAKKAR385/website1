1a0bcbef66bed1305fdf1cd04012441256e72243
KARANKAKKAR385 <143103376+KARANKAKKAR385@users.noreply.github.com>
2024-03-14 21:26:13 +0000

    Thu, Mar 14, 2024, 2:26 PM -07:00

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,18 @@
-loadText = document.querySelector('.loading-text');
+loadText = document.querySelector('.loading-text')
 bg = document.querySelector('.bg')
-load = 90
 
-loadText.innerText = `${load}%`
-loadText.style.style.opacity = 0.1
-bg.style.filter = "blur(10px)"
+load = 0
+
+int = setInterval(blurring, 30)
+
+function blurring() {
+    load++
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+
+
+    loadText.style.opacity = 1 - load/100
+    bg.style.filter = `blur(${30 - (load/100)*30}px)`
 }
-
